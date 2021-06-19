35304f5812c2646741b977eeb2f3021f83391b91
khabib <oemampedia@gmail.com>
2021-06-19 18:44:21 +0700

    delete middleware for endpoint /check-version

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,6 @@ import { isAuthrorize, isAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/check_version", checkVersion);
+router.get("/check-version", checkVersion);
 
 export default router;
